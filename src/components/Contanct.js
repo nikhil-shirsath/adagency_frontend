066ca79dd4e7ad50c0f985a7390c3eae7ca471f6
nikhil shirsath <001nikhilshirsath001@gmail.com>
2024-08-08 12:14:03 +0530

    intilial components

---
+++ src/components/Contanct.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contanct = () => {
+  return (
+    <div>
+      <h1>Our Contancts</h1>
+      <p>Details about the services we offer...</p>
+    </div>
+  );
+};
+
+export default Contanct;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home =()=>{
+return(
+    <div>
+    <h1>Home component </h1>
+    <p> this is home  component </p>
+    </div>
+);
+   
+}
+
+export default Home;
