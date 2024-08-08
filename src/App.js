dec424059df3b92bc58332fb7e4c26062ff94f63
nikhil shirsath <001nikhilshirsath001@gmail.com>
2024-08-08 13:29:45 +0530

    redirecting pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contanct from "./components/Contanct";
+import Service from "./components/Service";
+import UserLogin from "./components/UserLogin";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/service">Service</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contanct</Link></li>
+            <li><Link to="/userlogin">Login</Link></li>
+
+          </ul>
+        </nav>
+        <Routes>    
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/service" element={<Service/>} />
+                    <Route path="/about" element={<About/>} />
+                    <Route path="/contact" element={<Contanct/>} />
+                    <Route path="/userlogin" element={<UserLogin/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/UserLogin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserLogin = () => {
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <p>Details about the services we offer...</p>
+    </div>
+  );
+};
+
+export default UserLogin;
